35e29adcce71c46ba119481c7bb34ee7dd96f2c9
milonite <milonite@hotmail.it>
2021-12-28 14:41:46 +0100

    fix counter

--- src/constants.ts
+++ src/constants.ts
@@ -1,21 +1,21 @@
-export const IndexCoopAdapter = "0x9c9326C521895c78414BD3C2945e47AFC4Ef16cc";
-export const IndexedAdapter = "0x8F516c0FB0bcF44CFc8D42D60dAD3E4cdb35Ce26";
-export const PowerPoolAdapter = "0x0a883a62660328EAd442EB6c2311668ba9c12e57";
-export const TokenSetAdapter = "0x5F2C716D912Ce400a8A49fB87DB722e8257257A7";
-export const DHedgeAdapter = "0xaE6859311c341bAc4E0BcEcA0242247c16718FF1";
-export const PieDaoAdapter = "0xe149B1f2Ef564A289dc094215835f3c8Df1695aa";
+export const IndexCoopAdapter = "0x9c9326c521895c78414bd3c2945e47afc4ef16cc";
+export const IndexedAdapter = "0x8f516c0fb0bcf44cfc8d42d60dad3e4cdb35ce26";
+export const PowerPoolAdapter = "0x0a883a62660328ead442eb6c2311668ba9c12e57";
+export const TokenSetAdapter = "0x5f2c716d912ce400a8a49fb87db722e8257257a7";
+export const DHedgeAdapter = "0xae6859311c341bac4e0bceca0242247c16718ff1";
+export const PieDaoAdapter = "0xe149b1f2ef564a289dc094215835f3c8df1695aa";
 export const LiquidityMigration = "0x0092DECCA5E2f26466289011ad41465763BeA4cE";
 export const ERC1155 = "0x3a1c88addeC56e725ba2a432bC89eeaA5D068753";
 export const Claimable = "0xE39d9712F3749b9663627331d9BCa15Ac69b6170";
 
 export let adapters = new Array<string>();
 adapters = [
-  "0x9c9326C521895c78414BD3C2945e47AFC4Ef16cc",
-  "0x8F516c0FB0bcF44CFc8D42D60dAD3E4cdb35Ce26",
-  "0x0a883a62660328EAd442EB6c2311668ba9c12e57",
-  "0x5F2C716D912Ce400a8A49fB87DB722e8257257A7",
-  "0xaE6859311c341bAc4E0BcEcA0242247c16718FF1",
-  "0xe149B1f2Ef564A289dc094215835f3c8Df1695aa",
+  "0x9c9326c521895c78414bd3c2945e47afc4ef16cc",
+  "0x8f516c0fb0bcf44cfc8d42d60dad3e4cdb35ce26",
+  "0x0a883a62660328ead442eb6c2311668ba9c12e57",
+  "0x5f2c716d912ce400a8a49fb87db722e8257257a7",
+  "0xae6859311c341bac4e0bceca0242247c16718ff1",
+  "0xe149b1f2ef564a289dc094215835f3c8df1695aa",
 ];
 
 export let adaptersNames = new Array<string>();
--- src/mapping.ts
+++ src/mapping.ts
@@ -5,27 +5,28 @@ import {
 import { createStakedEvent } from "./entities/stakedEvent";
 import { Adapter } from "../generated/schema";
 import { adapters, adaptersNames } from "./constants";
+import { log } from "@graphprotocol/graph-ts";
 
 export function handleStaked(event: Staked): void {
   createStakedEvent(event);
 
-  let adapter = Adapter.load(event.params.adapter.toHexString());
-  if (adapter) {
-    adapter.staked = adapter.staked + 1;
-    adapter.save();
+  let adapter = Adapter.load(event.params.adapter.toHex());
+  if (adapter === null) {
+    return;
   }
+  adapter.staked = adapter.staked + 1;
+  adapter.save();
 }
 
 // This is the first event in the contract. It will be used to create Adapters entities
 export function handleOwnershipTransferred(event: OwnershipTransferred): void {
   for (let i = 0; i < adapters.length; i++) {
     let adapter = Adapter.load(adapters[i]);
-    if (adapter != null) {
-      return;
+    if (!adapter) {
+      adapter = new Adapter(adapters[i]);
+      adapter.name = adaptersNames[i];
+      adapter.staked = 0;
+      adapter.save();
     }
-    adapter = new Adapter(adapters[i]);
-    adapter.name = adaptersNames[i];
-    adapter.staked = 0;
-    adapter.save();
   }
 }
